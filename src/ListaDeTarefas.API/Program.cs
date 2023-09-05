3943fe095d888e870f69fda41f5b8d717aaea9bf
Lucas Lima <58302967+LucasViniLima@users.noreply.github.com>
2023-09-05 11:02:56 -0300

    adicionando extensao de infraestrutura banco de dados (#17)

--- src/ListaDeTarefas.API/Program.cs
+++ src/ListaDeTarefas.API/Program.cs
@@ -4,6 +4,7 @@ using ListaDeTarefas.Domain.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddInfrastructure(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
