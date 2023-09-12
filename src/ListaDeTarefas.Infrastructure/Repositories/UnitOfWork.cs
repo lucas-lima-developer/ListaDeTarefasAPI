c28f3b3554a2c325b60df83632976c6c088bc5d9
Lucas Lima <lucasViniLima15@gmail.com>
2023-09-12 15:22:46 -0300

    adiciionando dispose na unidade de trabalho

--- src/ListaDeTarefas.Infrastructure/Repositories/UnitOfWork.cs
+++ src/ListaDeTarefas.Infrastructure/Repositories/UnitOfWork.cs
@@ -3,9 +3,10 @@ using ListaDeTarefas.Infrastructure.Context;
 
 namespace ListaDeTarefas.Infrastructure.Repositories
 {
-    public class UnitOfWork : IUnitOfWork
+    public class UnitOfWork : IUnitOfWork, IDisposable
     {
         private readonly AppDbContext _context;
+        private bool _disposed;
 
         public UnitOfWork(AppDbContext context) 
         {
@@ -15,6 +16,21 @@ namespace ListaDeTarefas.Infrastructure.Repositories
         public async Task Commit()
         {
              await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!_disposed && disposing)
+            {
+                _context.Dispose();
+            }
+
+            _disposed = true;
         }
     }
 }
