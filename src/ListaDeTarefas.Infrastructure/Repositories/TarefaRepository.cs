5b701f7daf3cc3d7bbc666edf2a32717c38690b5
Lucas Lima <lucasViniLima15@gmail.com>
2023-09-12 15:22:31 -0300

    adicionando repositorio de tarefas

---
+++ src/ListaDeTarefas.Infrastructure/Repositories/TarefaRepository.cs
@@ -0,0 +1,11 @@
+﻿using ListaDeTarefas.Domain.Entities;
+using ListaDeTarefas.Domain.Interfaces;
+using ListaDeTarefas.Infrastructure.Context;
+
+namespace ListaDeTarefas.Infrastructure.Repositories
+{
+    public class TarefaRepository : BaseRepository<Tarefa>, ITarefaRepository
+    {
+        public TarefaRepository(AppDbContext context) : base(context) { }
+    }
+}
