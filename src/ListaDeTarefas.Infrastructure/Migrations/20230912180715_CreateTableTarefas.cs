e8caff6fd4db6057538936e27611b32c8fa341ca
Lucas Lima <lucasViniLima15@gmail.com>
2023-09-12 15:23:02 -0300

    adicionando migrations iniciais

---
+++ src/ListaDeTarefas.Infrastructure/Migrations/20230912180715_CreateTableTarefas.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ListaDeTarefas.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateTableTarefas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Description = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    CompletionDate = table.Column<DateTimeOffset>(type: "datetime(6)", nullable: true),
+                    Priority = table.Column<int>(type: "int", nullable: false),
+                    DateCreated = table.Column<DateTimeOffset>(type: "datetime(6)", nullable: false),
+                    DateUpdated = table.Column<DateTimeOffset>(type: "datetime(6)", nullable: true),
+                    DateDeleted = table.Column<DateTimeOffset>(type: "datetime(6)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tasks");
+        }
+    }
+}
