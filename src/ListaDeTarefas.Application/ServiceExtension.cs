46c2f8e2616e8f5e4b8eec03a6c552ddf45a9559
Lucas Lima <lucasViniLima15@gmail.com>
2023-09-12 15:23:19 -0300

    adicionando interface do repositorio de tarefas

---
+++ src/ListaDeTarefas.Application/ServiceExtension.cs
@@ -0,0 +1,18 @@
+﻿using ListaDeTarefas.Application.UseCases.TarefaUseCase.Add;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ListaDeTarefas.Application
+{
+    public static class ServiceExtension
+    {
+        public static void AddApplication(this IServiceCollection services)
+        {
+            AddUseCases(services);
+        }
+
+        private static void AddUseCases(IServiceCollection service)
+        {
+            service.AddScoped<IAddTarefaUseCase, AddTarefaUseCase>();
+        }
+    }
+}
