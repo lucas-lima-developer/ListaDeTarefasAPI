34243a037ad0e611877af21bfa9f8f64c8815c64
Lucas Lima <lucasViniLima15@gmail.com>
2023-09-16 16:56:52 -0300

    adicionando o caso de uso que retorna uma tarefa por id

---
+++ src/ListaDeTarefas.Application/UseCases/TarefaUseCase/GetById/GetByIdTarefaUseCase.cs
@@ -0,0 +1,27 @@
+﻿using ListaDeTarefas.Domain.Entities;
+using ListaDeTarefas.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaDeTarefas.Application.UseCases.TarefaUseCase.GetById
+{
+    public class GetByIdTarefaUseCase : IGetByIdTarefaUseCase
+    {
+        private readonly ITarefaRepository _tarefaRepository;
+
+        public GetByIdTarefaUseCase(ITarefaRepository tarefaRepository)
+        {
+            _tarefaRepository = tarefaRepository;
+        }
+
+        public async Task<Tarefa> Execute(long id)
+        {
+            var tarefa = await _tarefaRepository.Get(id);
+
+            return tarefa;
+        }
+    }
+}
