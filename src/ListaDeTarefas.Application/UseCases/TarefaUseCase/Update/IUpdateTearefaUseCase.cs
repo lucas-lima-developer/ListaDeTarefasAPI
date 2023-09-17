f74f167947b3e164765bb404e3ebbbf5543d9da9
Lucas Lima <lucasViniLima15@gmail.com>
2023-09-17 16:09:17 -0300

    implememntacao de caso de uso que atualiza tarefa

---
+++ src/ListaDeTarefas.Application/UseCases/TarefaUseCase/Update/IUpdateTearefaUseCase.cs
@@ -0,0 +1,9 @@
+﻿using ListaDeTarefas.Domain.Entities;
+
+namespace ListaDeTarefas.Application.UseCases.TarefaUseCase.Update
+{
+    public interface IUpdateTearefaUseCase
+    {
+        Task Execute(long id, Tarefa tarefa);
+    }
+}
