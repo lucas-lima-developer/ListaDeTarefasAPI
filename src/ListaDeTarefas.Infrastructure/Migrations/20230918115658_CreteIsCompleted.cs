a0ca427e305725ea6d743b94e189486948119bdc
Lucas Lima <lucasViniLima15@gmail.com>
2023-09-18 09:26:49 -0300

    migration removendo a data de exclusao e adicionando flag isCompleted

---
+++ src/ListaDeTarefas.Infrastructure/Migrations/20230918115658_CreteIsCompleted.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ListaDeTarefas.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreteIsCompleted : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateDeleted",
+                table: "Tarefas");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCompleted",
+                table: "Tarefas",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsCompleted",
+                table: "Tarefas");
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "DateDeleted",
+                table: "Tarefas",
+                type: "datetime(6)",
+                nullable: true);
+        }
+    }
+}
