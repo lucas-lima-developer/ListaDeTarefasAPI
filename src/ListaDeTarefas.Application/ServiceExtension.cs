6b6a6f932b5fd5a9d281ac7c52d0228e336af4cd
Lucas Lima <lucasViniLima15@gmail.com>
2023-09-29 01:00:51 -0300

    refatorando caso de uso que acha a tarefa por id e excluindo os outros casos de uso o de atualizar e deletar

--- src/ListaDeTarefas.Application/ServiceExtension.cs
+++ src/ListaDeTarefas.Application/ServiceExtension.cs
@@ -1,8 +1,5 @@
 ﻿using FluentValidation;
 using ListaDeTarefas.Application.Shared.Behavior;
-using ListaDeTarefas.Application.UseCases.TarefaUseCase.Delete;
-using ListaDeTarefas.Application.UseCases.TarefaUseCase.GetById;
-using ListaDeTarefas.Application.UseCases.TarefaUseCase.Update;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
@@ -17,21 +14,6 @@ namespace ListaDeTarefas.Application
             services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
             services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
-
-            AddUseCases(services);
-            AddValidators(services);
-        }
-
-        private static void AddUseCases(IServiceCollection service)
-        {
-            service.AddScoped<IDeleteTarefaUseCase, DeleteTarefaUseCase>();
-            service.AddScoped<IGetByIdTarefaUseCase, GetByIdTarefaUseCase>();
-            service.AddScoped<IUpdateTearefaUseCase, UpdateTarefaUseCase>();
-        }
-
-        private static void AddValidators(IServiceCollection services)
-        {
-            services.AddTransient<IValidator<UpdateTarefaRequest>, UpdateTarefaRequestValidator>();
         }
     }
 }
--- src/ListaDeTarefas.Application/UseCases/TarefaUseCase/Update/IUpdateTearefaUseCase.cs
+++
@@ -1,9 +0,0 @@
-﻿using ListaDeTarefas.Domain.Entities;
-
-namespace ListaDeTarefas.Application.UseCases.TarefaUseCase.Update
-{
-    public interface IUpdateTearefaUseCase
-    {
-        Task<Tarefa> Execute(UpdateTarefaRequest tarefa);
-    }
-}
