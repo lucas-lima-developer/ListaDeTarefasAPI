e8073d0d8c6db63507117d2830e33084c2a64b20
Lucas Lima <lucasViniLima15@gmail.com>
2023-10-04 23:15:51 -0300

    criando nova exceção quando a tarefa não é encontrada

---
+++ src/ListaDeTarefas.Application/Exceptions/TarefaNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿namespace ListaDeTarefas.Application.Exceptions
+{
+    public class TarefaNotFoundException : Exception
+    {
+        private long TarefaId { get; }
+
+        public TarefaNotFoundException(long tarefaId) : base($"A tarefa com ID = {tarefaId} não foi encontrada.") 
+        {
+            TarefaId = tarefaId;
+        }
+
+        public TarefaNotFoundException(string message) : base(message) { }
+    }
+}
