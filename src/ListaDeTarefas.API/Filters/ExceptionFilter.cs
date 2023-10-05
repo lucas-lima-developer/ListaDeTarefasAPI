4d11184f98251fe8644fcd4bdd364501945b0bf9
Lucas Lima <lucasViniLima15@gmail.com>
2023-10-04 23:16:03 -0300

    adicionando exceção no filtro

--- src/ListaDeTarefas.API/Filters/ExceptionFilter.cs
+++ src/ListaDeTarefas.API/Filters/ExceptionFilter.cs
@@ -1,4 +1,5 @@
 ﻿using ListaDeTarefas.API.Contracts;
+using ListaDeTarefas.Application.Exceptions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
@@ -18,6 +19,16 @@ namespace ListaDeTarefas.API.Filters
                 };
 
                 context.Result = new JsonResult(error) { StatusCode = 400 };
+            }
+            else if (context.Exception is TarefaNotFoundException)
+            {
+                error = new Error
+                {
+                    StatusCode = "404",
+                    Message = context.Exception.Message,
+                };
+
+                context.Result = new JsonResult(error) { StatusCode = 404 };
             }
             else
             {
