626e27f03580b1531e126c335f0e24eddc375592
Lucas Lima <lucasViniLima15@gmail.com>
2023-10-05 13:02:40 -0300

    criando nova exceção para quando ocorrer erro de validação

---
+++ src/ListaDeTarefas.Application/Exceptions/ValidationErrorException.cs
@@ -0,0 +1,17 @@
+﻿namespace ListaDeTarefas.Application.Exceptions
+{
+    public class ValidationErrorException : Exception
+    {
+        public IEnumerable<string> Errors {  get; }
+
+        public ValidationErrorException(IEnumerable<string> errors) : base(BuildMessage(errors))
+        {
+            Errors = errors;
+        }
+
+        private static string BuildMessage(IEnumerable<string> errors)
+        {
+            return string.Join(" || ", errors);
+        }
+    }
+}
