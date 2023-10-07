32c1202d34a9e20c6888810a0422e297fcb5bab8
Lucas Lima <lucasViniLima15@gmail.com>
2023-10-07 11:11:33 -0300

    adicionando helper que vai definir se a priority é válida

---
+++ src/ListaDeTarefas.Application/Helpers/ValidationHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace ListaDeTarefas.Application.Helpers
+{
+    public static class ValidationHelper
+    {
+        public static bool BeAValidPriority(string priority)
+        {
+            return priority == "baixa" || priority == "média" || priority == "alta";
+        }
+    }
+}
