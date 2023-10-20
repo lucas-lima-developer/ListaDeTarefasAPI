c4f6bc4c07a99ba9cb19cf99625b9b50b9bf353e
Lucas Lima <lucasViniLima15@gmail.com>
2023-10-20 08:33:42 -0300

    criando interface do usecase de getall tarefa

---
+++ src/ListaDeTarefas.Application/UseCases/GetAllTarefa/IGetAllTarefaUseCase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaDeTarefas.Application.UseCases.GetAllTarefa
+{
+    public interface IGetAllTarefaUseCase
+    {
+        Task<List<GetAllTarefaResponse>> Execute(CancellationToken cancellationToken);
+    }
+}
