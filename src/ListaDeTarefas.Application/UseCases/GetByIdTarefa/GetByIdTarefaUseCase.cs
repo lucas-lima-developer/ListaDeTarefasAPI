6a672de37b355e5dd45c34d21e35ad49b6f5faa9
Lucas Lima <lucasViniLima15@gmail.com>
2023-10-20 09:27:14 -0300

    adicionando caso de uso de retornar tarefa pelo id

---
+++ src/ListaDeTarefas.Application/UseCases/GetByIdTarefa/GetByIdTarefaUseCase.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using ListaDeTarefas.Application.Exceptions;
+using ListaDeTarefas.Domain.Interfaces;
+
+namespace ListaDeTarefas.Application.UseCases.GetByIdTarefa
+{
+    public class GetByIdTarefaUseCase : IGetByIdTarefaUseCase
+    {
+        private readonly ITarefaRepository _tarefaRepository;
+        private readonly IMapper _mapper;
+
+        public GetByIdTarefaUseCase(ITarefaRepository tarefaRepository, IMapper mapper)
+        {
+            _tarefaRepository = tarefaRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<GetByIdTarefaResponse> Execute(int id, CancellationToken cancellationToken)
+        {
+            var tarefa = await _tarefaRepository.GetById(id, cancellationToken);
+
+            if (tarefa == null) throw new TarefaNotFoundException(id);
+
+            return _mapper.Map<GetByIdTarefaResponse>(tarefa);
+        }
+    }
+}
