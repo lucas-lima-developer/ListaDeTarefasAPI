815540881656f77a0f5726c71b49b0147f99590b
Lucas Lima <lucasViniLima15@gmail.com>
2023-10-20 09:26:40 -0300

    adicionando response do caso de uso de retornar tarefa pelo id

---
+++ src/ListaDeTarefas.Application/UseCases/GetByIdTarefa/GetByIdTarefaResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace ListaDeTarefas.Application.UseCases.GetByIdTarefa
+{
+    public class GetByIdTarefaResponse
+    {
+        public long Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime? LimitDate { get; set; }
+        public string? Priority { get; set; }
+        public bool? IsCompleted { get; set; }
+        public DateTime? CompletionDate { get; set; }
+        public DateTime? DateCreated { get; set; }
+        public DateTime? DateUpdated { get; set; }
+    }
+}
