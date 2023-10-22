4fc6e354107275112434dc165ac3e67bed260d56
Lucas Lima <lucasViniLima15@gmail.com>
2023-10-22 20:05:35 -0300

    adicionando o caso de uso que cadastra atividade

---
+++ src/ListaDeTarefas.Application/UseCases/CreateTarefa/ICreateTarefaUseCase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaDeTarefas.Application.UseCases.CreateTarefa
+{
+    public interface ICreateTarefaUseCase
+    {
+        Task<CreateTarefaResponse> Execute(CreateTarefaRequest request, CancellationToken cancellationToken);
+    }
+}
