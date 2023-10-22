5fd27723dd6a003f3b55e015447a11aba93ea9dd
Lucas Lima <lucasViniLima15@gmail.com>
2023-10-22 20:15:30 -0300

    adicionado mapper

---
+++ src/ListaDeTarefas.Application/UseCases/DeleteTarefa/DeleteTarefaMapper.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using ListaDeTarefas.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaDeTarefas.Application.UseCases.DeleteTarefa
+{
+    public sealed class DeleteTarefaMapper : Profile
+    {
+        public DeleteTarefaMapper()
+        {
+            CreateMap<Tarefa, DeleteTarefaResponse>();
+        }
+    }
+}
