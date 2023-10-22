ea361bfb615e96e8ccf91c81b2b0348f69600e67
Lucas Lima <lucasViniLima15@gmail.com>
2023-10-22 20:05:56 -0300

    criando request

---
+++ src/ListaDeTarefas.Application/UseCases/CreateTarefa/CreateTarefaRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace ListaDeTarefas.Application.UseCases.CreateTarefa
+{
+    public sealed class CreateTarefaRequest 
+    { 
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime LimitDate { get; set; }
+        public string? Priority { get; set; }
+    }
+}
