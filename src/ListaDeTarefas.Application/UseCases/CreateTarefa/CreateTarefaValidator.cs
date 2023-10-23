77da382539b9cde4d2f1e97918bedd922379fdef
Lucas Lima <lucasViniLima15@gmail.com>
2023-10-23 18:36:18 -0300

    adicionado validação para o request de criar tarefa

---
+++ src/ListaDeTarefas.Application/UseCases/CreateTarefa/CreateTarefaValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace ListaDeTarefas.Application.UseCases.CreateTarefa
+{
+    public class CreateTarefaValidator : AbstractValidator<CreateTarefaRequest>
+    {
+        public CreateTarefaValidator() 
+        {
+            RuleFor(x => x.Title)
+                .NotEmpty().WithMessage(Exceptions.Resources.ErrorMessages.TITLE_NOT_EMPTY)
+                .MaximumLength(50).WithMessage(Exceptions.Resources.ErrorMessages.TITLE_MAX_LENGTH);
+            RuleFor(x => x.Description)
+                .NotEmpty().WithMessage(Exceptions.Resources.ErrorMessages.DESCRIPTION_NOT_EMPTY)
+                .MaximumLength(150).WithMessage(Exceptions.Resources.ErrorMessages.DESCRIPTION_MAX_LENGTH);
+        }
+    }
+}
