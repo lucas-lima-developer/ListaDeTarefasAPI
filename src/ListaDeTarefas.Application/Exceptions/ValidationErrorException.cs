d4ddbeda1cb66fee019c4c01631b1fd7d33b5645
Lucas Lima <lucasViniLima15@gmail.com>
2023-10-23 18:37:00 -0300

    refatoração da classes que é responsável pelas exceções de validação

--- src/ListaDeTarefas.Application/Exceptions/ValidationErrorException.cs
+++ src/ListaDeTarefas.Application/Exceptions/ValidationErrorException.cs
@@ -1,19 +1,11 @@
-﻿namespace ListaDeTarefas.Application.Exceptions
+﻿using FluentValidation.Results;
+
+namespace ListaDeTarefas.Application.Exceptions
 {
     public class ValidationErrorException : Exception
     {
-        public IEnumerable<string>? Errors {  get; }
-
-        public ValidationErrorException(IEnumerable<string> errors) : base(BuildMessage(errors))
-        {
-            Errors = errors;
-        }
-
         public ValidationErrorException(string message) : base(message) { }
 
-        private static string BuildMessage(IEnumerable<string> errors)
-        {
-            return string.Join(" || ", errors);
-        }
+        public ValidationErrorException(ValidationResult result) : base(result.ToString(" ")) { }
     }
 }
