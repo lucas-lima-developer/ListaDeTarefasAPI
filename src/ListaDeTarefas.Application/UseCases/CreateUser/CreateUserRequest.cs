8273e5e12d9ee4bea743bffa6ef9376f46e7b1c8
Lucas Lima <lucasViniLima15@gmail.com>
2023-10-30 22:26:10 -0300

    adicionando caso de uso de criar usuário

---
+++ src/ListaDeTarefas.Application/UseCases/CreateUser/CreateUserRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace ListaDeTarefas.Application.UseCases.CreateUser
+{
+    public class CreateUserRequest
+    {
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+    }
+}
---
+++ src/ListaDeTarefas.Application/UseCases/CreateUser/ICreateUserUseCase.cs
@@ -0,0 +1,7 @@
+﻿namespace ListaDeTarefas.Application.UseCases.CreateUser
+{
+    public interface ICreateUserUseCase
+    {
+        Task<CreateUserResponse> Execute(CreateUserRequest request, CancellationToken cancellationToken);
+    }
+}
