719149812ea02a2450597feb128931e7d1390901
Lucas Lima <lucasViniLima15@gmail.com>
2023-10-30 22:25:09 -0300

    adicionando repositório do usuário

---
+++ src/ListaDeTarefas.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,24 @@
+﻿using ListaDeTarefas.Domain.Entities;
+using ListaDeTarefas.Domain.Interfaces;
+using ListaDeTarefas.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace ListaDeTarefas.Infrastructure.Repositories
+{
+    public class UserRepository : BaseRepository<User>, IUserRepository
+    {
+        private readonly AppDbContext _context;
+
+        public UserRepository(AppDbContext context) : base(context) 
+        {
+            _context = context;
+        }
+
+        public async Task<User?> GetByEmail(string email, CancellationToken cancellationToken)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Email == email);
+
+            return user;
+        }
+    }
+}
