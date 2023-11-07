972d3d2e5b5bd35746865b1b4dfee58f839f264e
Lucas Lima <lucasViniLima15@gmail.com>
2023-11-06 23:12:35 -0300

    modificando caso de uso para atualizar apenas as atividades do usuário

--- src/ListaDeTarefas.Application/UseCases/UpdateTarefa/UpdateTarefaUseCase.cs
+++ src/ListaDeTarefas.Application/UseCases/UpdateTarefa/UpdateTarefaUseCase.cs
@@ -11,29 +11,33 @@ namespace ListaDeTarefas.Application.UseCases.UpdateTarefa
     {
 
         private readonly ITarefaRepository _tarefaRepository;
+        private readonly IUserRepository _userRepository;
         private readonly IUnitOfWork _unitOfWorK;
         private readonly IMapper _mapper;
         private readonly IValidator<UpdateTarefaRequest> _validator;
 
-        public UpdateTarefaUseCase(ITarefaRepository tarefaRepository, IUnitOfWork unitOfWorK, IMapper mapper, IValidator<UpdateTarefaRequest> validator)
+        public UpdateTarefaUseCase(ITarefaRepository tarefaRepository, IUserRepository userRepository, IUnitOfWork unitOfWorK, IMapper mapper, IValidator<UpdateTarefaRequest> validator)
         {
             _tarefaRepository = tarefaRepository;
+            _userRepository = userRepository;
             _unitOfWorK = unitOfWorK;
             _mapper = mapper;
             _validator = validator;
         }
 
-        public async Task<UpdateTarefaResponse> Execute(UpdateTarefaRequest request, CancellationToken cancellationToken)
+        public async Task<UpdateTarefaResponse> Execute(UpdateTarefaRequest request, string email, CancellationToken cancellationToken)
         {
             var result = await _validator.ValidateAsync(request, cancellationToken);
 
-            if (!result.IsValid)
-                throw new ValidationErrorException(result);
+            if (!result.IsValid) throw new ValidationErrorException(result);
 
-            var tarefa = await _tarefaRepository.GetById(request.Id, cancellationToken);
+            var user = await _userRepository.GetByEmail(email, cancellationToken);
 
-            if (tarefa == null)
-                throw new TarefaNotFoundException(request.Id);
+            if (user is null) throw new Exception("Usuário não encontrado");
+
+            var tarefa = await _tarefaRepository.GetById(user, request.Id, cancellationToken);
+
+            if (tarefa is null) throw new TarefaNotFoundException(request.Id);
 
             _mapper.Map(request, tarefa);
 
