b85d3a34634fb6d739e0bee70d63d0a37955649b
Lucas Lima <lucasViniLima15@gmail.com>
2023-11-06 23:11:09 -0300

    modificando o caso de uso que retorna todas as tarefas para retornar apenas a do user

--- src/ListaDeTarefas.Application/UseCases/GetAllTarefa/GetAllTarefaUseCase.cs
+++ src/ListaDeTarefas.Application/UseCases/GetAllTarefa/GetAllTarefaUseCase.cs
@@ -8,19 +8,23 @@ namespace ListaDeTarefas.Application.UseCases.GetAllTarefa
     public class GetAllTarefaUseCase : IGetAllTarefaUseCase
     {
         private readonly ITarefaRepository _tarefaRepository;
+        private readonly IUserRepository _userRepository;
         private readonly IMapper _mapper;
 
-        public GetAllTarefaUseCase(ITarefaRepository tarefaRepository, IMapper mapper)
+        public GetAllTarefaUseCase(ITarefaRepository tarefaRepository, IUserRepository userRepository, IMapper mapper)
         {
             _tarefaRepository = tarefaRepository;
+            _userRepository = userRepository;
             _mapper = mapper;
         }
 
-        public async Task<List<GetAllTarefaResponse>> Execute(CancellationToken cancellationToken)
+        public async Task<List<GetAllTarefaResponse>> Execute(string emailUser, CancellationToken cancellationToken)
         {
-            var tarefas = await _tarefaRepository.GetAll(cancellationToken);
+            var user = await _userRepository.GetByEmail(emailUser, cancellationToken);
 
-            if (tarefas.Count == 0) throw new TarefaNotFoundException("Nenhuma tarefa encontrada.");
+            var tarefas = await _tarefaRepository.GetAll(user!, cancellationToken);
+
+            if (tarefas?.Count == 0) throw new TarefaNotFoundException("Nenhuma tarefa encontrada.");
 
             return _mapper.Map<List<GetAllTarefaResponse>>(tarefas);
         }
