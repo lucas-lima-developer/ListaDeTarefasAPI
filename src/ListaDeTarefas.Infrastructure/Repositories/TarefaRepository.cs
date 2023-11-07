e59b0c1be3dbc5ae133f800003780fb5366d4551
Lucas Lima <lucasViniLima15@gmail.com>
2023-11-06 23:08:09 -0300

    adicionando sobrecarga em metodos que retornan a tarefa de determinado usuário

--- src/ListaDeTarefas.Infrastructure/Repositories/TarefaRepository.cs
+++ src/ListaDeTarefas.Infrastructure/Repositories/TarefaRepository.cs
@@ -1,11 +1,27 @@
 ﻿using ListaDeTarefas.Domain.Entities;
 using ListaDeTarefas.Domain.Interfaces;
 using ListaDeTarefas.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace ListaDeTarefas.Infrastructure.Repositories
 {
     public class TarefaRepository : BaseRepository<Tarefa>, ITarefaRepository
     {
-        public TarefaRepository(AppDbContext context) : base(context) { }
+        private readonly AppDbContext _context;
+
+        public TarefaRepository(AppDbContext context) : base(context) 
+        {
+            _context = context;
+        }
+
+        public async Task<List<Tarefa>> GetAll(User user, CancellationToken cancellationToken)
+        {
+            return await _context.Tarefas.Where(tarefa => tarefa.User == user).ToListAsync(cancellationToken);
+        }
+
+        public async Task<Tarefa?> GetById(User user, long id, CancellationToken cancellationToken)
+        {
+            return await _context.Tarefas.FirstOrDefaultAsync(tarefa => tarefa.User == user && tarefa.Id == id, cancellationToken);
+        }
     }
 }
