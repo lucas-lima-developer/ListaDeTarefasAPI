a0f0208ffaecfda7f1db1c1a7b0dfa21957ccd4b
Lucas Lima <lucasViniLima15@gmail.com>
2023-11-07 11:23:15 -0300

    adicionando exception quando usuário não é encontrado

---
+++ src/ListaDeTarefas.Application/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,7 @@
+﻿namespace ListaDeTarefas.Application.Exceptions
+{
+    public class UserNotFoundException : Exception
+    {
+        public UserNotFoundException() : base(Exceptions.Resources.ErrorMessages.USER_NOT_FOUND) { }
+    }
+}
