3e90d806e6ea260be2524e6356da47c5aa544a4a
Lucas Lima <lucasViniLima15@gmail.com>
2023-11-08 22:30:51 -0300

    adicionando di dos validators de criar e login de usuário

--- src/ListaDeTarefas.Application/UseCases/LoginUser/LoginUserUseCase.cs
+++ src/ListaDeTarefas.Application/UseCases/LoginUser/LoginUserUseCase.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using FluentValidation;
 using ListaDeTarefas.Application.Exceptions;
 using ListaDeTarefas.Application.Requests;
 using ListaDeTarefas.Application.Responses;
@@ -17,16 +18,21 @@ namespace ListaDeTarefas.Application.UseCases.LoginUser
         private readonly IUserRepository _userRepository;
         private readonly IMapper _mapper;
         private readonly IConfiguration _configuration;
+        private readonly IValidator<LoginUserRequest> _validator;
 
-        public LoginUserUseCase(IUserRepository userRepository, IMapper mapper, IConfiguration configuration)
+        public LoginUserUseCase(IUserRepository userRepository, IMapper mapper, IConfiguration configuration, IValidator<LoginUserRequest> validator)
         {
             _userRepository = userRepository;
             _mapper = mapper;
             _configuration = configuration;
+            _validator = validator;
         }
 
         public async Task<LoginUserResponse> Execute(LoginUserRequest request, CancellationToken cancellationToken)
         {
+            var result = await _validator.ValidateAsync(request, cancellationToken);
+            if (!result.IsValid) throw new ValidationErrorException(result);
+
             var user = await _userRepository.GetByEmail(request.Email!, cancellationToken);
             if (user == null) throw new WrongCredentialsException();
 
