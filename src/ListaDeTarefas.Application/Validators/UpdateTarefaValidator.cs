96b754e23792c8d44be5419cb078f3eca690c071
Lucas Lima <lucasViniLima15@gmail.com>
2023-11-09 10:31:47 -0300

    aplicando novos nomes no validador de atualizar tarefa

--- src/ListaDeTarefas.Application/Validators/UpdateTarefaValidator.cs
+++ src/ListaDeTarefas.Application/Validators/UpdateTarefaValidator.cs
@@ -10,13 +10,13 @@ namespace ListaDeTarefas.Application.Validators
             RuleFor(tarefa => tarefa.Id)
                 .NotEmpty().WithMessage(Exceptions.Resources.ErrorMessages.ID_EMPTY);
             RuleFor(tarefa => tarefa.Title)
-                .NotEmpty().WithMessage(Exceptions.Resources.ErrorMessages.TITLE_NOT_EMPTY)
-                .MaximumLength(50).WithMessage(Exceptions.Resources.ErrorMessages.TITLE_MAX_LENGTH);
+                .NotEmpty().WithMessage(Exceptions.Resources.ErrorMessages.TAREFA_TITLE_EMPTY)
+                .MaximumLength(50).WithMessage(Exceptions.Resources.ErrorMessages.TAREFA_TITLE_MAX_LENGTH);
             RuleFor(tarefa => tarefa.Description)
-                .NotEmpty().WithMessage(Exceptions.Resources.ErrorMessages.DESCRIPTION_NOT_EMPTY)
-                .MaximumLength(150).WithMessage(Exceptions.Resources.ErrorMessages.DESCRIPTION_MAX_LENGTH);
+                .NotEmpty().WithMessage(Exceptions.Resources.ErrorMessages.TAREFA_DESCRIPTION_EMPTY)
+                .MaximumLength(150).WithMessage(Exceptions.Resources.ErrorMessages.TAREFA_DESCRIPTION_MAX_LENGTH);
             RuleFor(tarefa => tarefa.IsCompleted)
-                .NotNull().WithMessage(Exceptions.Resources.ErrorMessages.IS_COMPLETED_NOT_EMPTY);
+                .NotNull().WithMessage(Exceptions.Resources.ErrorMessages.TAREFA_IS_COMPLETED_EMPTY);
         }
     }
 }
